3eb27f64e5ac565db2250a8d746d7c42cb55515f
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-04-17 13:34:01 +0200

    Reduced the skew of the rate slider

--- tremolo_plugin/source/Parameters.cpp
+++ tremolo_plugin/source/Parameters.cpp
@@ -6,7 +6,7 @@ juce::AudioParameterFloat& createModulationRateParameter(
   constexpr auto versionHint = 1;
   auto parameter = std::make_unique<juce::AudioParameterFloat>(
       juce::ParameterID{"modulation.rate", versionHint}, "Modulation rate",
-      juce::NormalisableRange<float>{0.1f, 20.f, 0.1f}, 5.f,
+      juce::NormalisableRange<float>{0.1f, 20.f, 0.1f, 0.1f}, 5.f,
       juce::AudioParameterFloatAttributes{}
           .withLabel("Hz")
           .withStringFromValueFunction(
