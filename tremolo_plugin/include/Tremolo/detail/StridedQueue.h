817dda08f5acc4750c15fb0e247a087132002e62
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-04-17 09:02:41 +0200

    WIP: Run-time tests of element count calculation

--- tremolo_plugin/include/Tremolo/detail/StridedQueue.h
+++ tremolo_plugin/include/Tremolo/detail/StridedQueue.h
@@ -17,23 +17,47 @@ public:
   float& at(size_t index) { return stridedElements.at(index); }
 
   void pushBack(const juce::AudioBuffer<float>& buffer) {
+    int _added = 0;
+    
+    const auto toBeAdded = newElementsCount(buffer.getNumSamples());
+
     for (; elementIndex < buffer.getNumSamples(); elementIndex += stride) {
       const auto sample = buffer.getSample(0, elementIndex);
       stridedElements.pop_front();
       stridedElements.push_back(sample);
+
+      _added++;
     }
+
+    jassert(toBeAdded == _added);
+
     elementIndex %= stride;
   }
 
   void pushBackZeros(int zerosCount) {
+    int _added = 0;
+    
+    const auto toBeAdded = newElementsCount(zerosCount);
     for (; elementIndex < zerosCount; elementIndex += stride) {
       stridedElements.pop_front();
       stridedElements.push_back(0.f);
+      _added++;
     }
+    jassert(toBeAdded == _added);
     elementIndex %= stride;
   }
 
 private:
+  [[nodiscard]] int newElementsCount(int sampleCount) const noexcept {
+    const auto lowerBound = sampleCount / stride;
+
+    if (lowerBound * stride + elementIndex < sampleCount) {
+      return lowerBound + 1;
+    }
+
+    return lowerBound;
+  }
+
   std::deque<float> stridedElements;
   int elementIndex{0};
   int stride{1};
