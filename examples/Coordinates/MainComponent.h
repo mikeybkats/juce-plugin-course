5b21c9139b707ee038a904a3950cf220b1c12588
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-06-17 09:55:25 +0200

    Finished the Coordinates demo

--- examples/Coordinates/MainComponent.h
+++ examples/Coordinates/MainComponent.h
@@ -4,5 +4,24 @@ class MainComponent : public juce::Component {
 public:
   MainComponent() { setBounds(0, 0, 400, 300); }
 
-  void paint(juce::Graphics& g) override { g.fillAll(juce::Colours::orange); }
+  void paint(juce::Graphics& g) override {
+    g.setColour(juce::Colours::red);
+    g.fillRect(rect1);
+    g.setColour(juce::Colours::blue);
+    g.fillRect(rect2);
+  }
+
+  void resized() override {
+    const auto bounds = getLocalBounds().toFloat();
+    const auto halfHeight = bounds.getHeight() / 2.f;
+
+    rect1 = juce::Rectangle<float>{bounds.getX(), bounds.getY(),
+                                   bounds.getWidth(), halfHeight};
+    rect2 = juce::Rectangle<float>{bounds.getX(), bounds.getY() + halfHeight,
+                                   bounds.getWidth(), halfHeight};
+  }
+
+private:
+  juce::Rectangle<float> rect1;
+  juce::Rectangle<float> rect2;
 };
